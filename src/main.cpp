d9400dd8f5648cfb103c1545dcd3ebcd82782de4
POISSON Clement <Clement.Poisson@bordeaux-inp.fr>
2023-05-25 14:43:51 -0300

    First ESP32 Upload (just HelloWorld)

--- src/main.cpp
+++ src/main.cpp
@@ -24,63 +24,71 @@ void setup()
   Serial.begin(115200);
   Serial.println("Hello World!");
 
-  // Load the sample sine model
-	Serial.println("Loading Tensorflow model....");
-	stop_model = tflite::GetModel(stop_model_data);
-	Serial.println("Sine model loaded!");
+  // // Load the sample sine model
+	// Serial.println("Loading Tensorflow model....");
+	// stop_model = tflite::GetModel(stop_model_data);
+	// Serial.println("Model loaded!");
 
+  // // Define ops resolver and error reporting
+	// static tflite::ops::micro::AllOpsResolver resolver;
+  // static tflite::ErrorReporter* error_reporter;
+	// static tflite::MicroErrorReporter micro_error;
+	// error_reporter = &micro_error;
 
-  // Define ops resolver and error reporting
-	static tflite::ops::micro::AllOpsResolver resolver;
-  static tflite::ErrorReporter* error_reporter;
-	static tflite::MicroErrorReporter micro_error;
-	error_reporter = &micro_error;
-
-  // Instantiate the interpreter 
-	static tflite::MicroInterpreter static_interpreter(stop_model, 
-                                                     resolver, 
-                                                     tensor_pool, 
-                                                     tensor_pool_size, 
-                                                     error_reporter);
+  // // Instantiate the interpreter 
+	// static tflite::MicroInterpreter static_interpreter(stop_model, 
+  //                                                    resolver, 
+  //                                                    tensor_pool, 
+  //                                                    tensor_pool_size, 
+  //                                                    error_reporter);
   
-  interpreter = &static_interpreter;
+  // interpreter = &static_interpreter;
 
-  // Allocate the the model's tensors in the memory pool that was created.
-	Serial.println("Allocating tensors to memory pool");
-	if(interpreter->AllocateTensors() != kTfLiteOk) {
-		Serial.println("There was an error allocating the memory...ooof");
-		return;
-	}
+  // // Allocate the the model's tensors in the memory pool that was created.
+	// Serial.println("Allocating tensors to memory pool");
+	// if(interpreter->AllocateTensors() != kTfLiteOk) {
+	// 	Serial.println("There was an error allocating the memory...ooof");
+	// 	return;
+  
+  Serial.println("Setup Complete");
 
-	// Define input and output nodes
-	input = interpreter->input(0);
-	output = interpreter->output(0);
-	Serial.println("Starting inferences... Input a number! ");
+	// }
+
+	// // Define input and output nodes
+	// input = interpreter->input(0);
+	// output = interpreter->output(0);
+	// Serial.println("Starting inferences... Input a number! ");
 }
 
 void loop() 
 {
-  // Wait for serial input to be made available and parse it as a float
-	if(Serial.available() > 0) {
-    float user_input = Serial.parseFloat();
+
+  Serial.println("Looping...");
+  delay(1000);
+
+
+
+  // // Wait for serial input to be made available and parse it as a float
+	// if(Serial.available() > 0) {
+  //   float user_input = Serial.parseFloat();
     	
-    // Set the input node to the user input
-    input->data.f[0] = user_input;
+  //   // Set the input node to the user input
+  //   input->data.f[0] = user_input;
 
-    Serial.println("Running inference on inputted data...");
+  //   Serial.println("Running inference on inputted data...");
 
-    // Run inference on the input data
-    if(interpreter->Invoke() != kTfLiteOk) {
-      Serial.println("There was an error invoking the interpreter!");
-      return;
-    }
+  //   // Run inference on the input data
+  //   if(interpreter->Invoke() != kTfLiteOk) {
+  //     Serial.println("There was an error invoking the interpreter!");
+  //     return;
+  //   }
 
-    // Print the output of the model.
-    Serial.print("Input: ");
-    Serial.println(user_input);
-    Serial.print("Output: ");
-    Serial.println(output->data.f[0]);
-    Serial.println("");
+  //   // Print the output of the model.
+  //   Serial.print("Input: ");
+  //   Serial.println(user_input);
+  //   Serial.print("Output: ");
+  //   Serial.println(output->data.f[0]);
+  //   Serial.println("");
 
-  }
+  // }
 }
